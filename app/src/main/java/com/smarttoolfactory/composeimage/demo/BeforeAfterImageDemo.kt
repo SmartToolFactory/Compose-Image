a400da165034ae8e6a8508b5733846f0c0a7e8b9
Smart Tool Factory <smarttoolfactory@icloud.com>
2022-07-19 12:13:57 +0300

    update BeforeAfterImageDemo

--- app/src/main/java/com/smarttoolfactory/composeimage/demo/BeforeAfterImageDemo.kt
+++ app/src/main/java/com/smarttoolfactory/composeimage/demo/BeforeAfterImageDemo.kt
@@ -1,31 +1,32 @@
 package com.smarttoolfactory.composeimage.demo
 
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.Image
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.imageResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.smarttoolfactory.composeimage.R
 import com.smarttoolfactory.image.beforeafter.BeforeAfterImage
-import com.smarttoolfactory.image.beforeafter.BeforeAfterLayout
+import com.smarttoolfactory.image.beforeafter.Order
+import kotlin.math.roundToInt
 
 @Composable
 fun BeforeAfterImageDemo() {
@@ -33,101 +34,129 @@ fun BeforeAfterImageDemo() {
         modifier = Modifier
             .fillMaxSize()
             .padding(10.dp)
+            .verticalScroll(rememberScrollState())
     ) {
         val imageBefore = ImageBitmap.imageResource(
-            LocalContext.current.resources, R.drawable.landscape5
+            LocalContext.current.resources, R.drawable.image_before_after_shoes_a
         )
 
         val imageAfter = ImageBitmap.imageResource(
-            LocalContext.current.resources, R.drawable.landscape5_after
+            LocalContext.current.resources, R.drawable.image_before_after_shoes_b
         )
 
-        val painter: Painter = painterResource(id = R.drawable.baseline_swap_horiz_24)
+        val imageBefore2 = ImageBitmap.imageResource(
+            LocalContext.current.resources, R.drawable.image_before_after_elements_a
+        )
 
-        val density = LocalDensity.current
+        val imageAfter2 = ImageBitmap.imageResource(
+            LocalContext.current.resources, R.drawable.image_before_after_elements_b
+        )
+
+        Text(
+            text = "BeforeAfterImage",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.padding(8.dp)
+        )
+
+        Box {
+            BeforeAfterImage(
+                modifier = Modifier
+                    .shadow(1.dp, RoundedCornerShape(10.dp))
+                    .fillMaxWidth()
+                    .aspectRatio(4 / 3f),
+                beforeImage = imageBefore,
+                afterImage = imageAfter,
+                contentScale = ContentScale.FillBounds
+            )
+            Label(
+                text = "BEFORE",
+                modifier = Modifier
+                    .padding(8.dp)
+                    .align(Alignment.TopStart)
+            )
+            Label(
+                text = "AFTER", modifier = Modifier
+                    .padding(8.dp)
+                    .align(Alignment.TopEnd)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Box {
+            BeforeAfterImage(
+                modifier = Modifier
+                    .shadow(1.dp, RoundedCornerShape(10.dp))
+                    .fillMaxWidth()
+                    .aspectRatio(4 / 3f),
+                beforeImage = imageBefore,
+                afterImage = imageAfter,
+                order = Order.AfterBefore,
+                contentScale = ContentScale.FillBounds
+            )
+            Label(
+                text = "AFTER",
+                modifier = Modifier
+                    .padding(8.dp)
+                    .align(Alignment.TopStart)
+            )
+            Label(
+                text = "BEFORE", modifier = Modifier
+                    .padding(8.dp)
+                    .align(Alignment.TopEnd)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        val transition: InfiniteTransition = rememberInfiniteTransition()
+
+        // Infinite progress animation
+        val progress by transition.animateFloat(
+            initialValue = 0f,
+            targetValue = 100f,
+            animationSpec = infiniteRepeatable(
+                animation = tween(
+                    durationMillis = 4000,
+                    easing = FastOutSlowInEasing
+                ),
+                repeatMode = RepeatMode.Reverse
+            )
+        )
 
         BeforeAfterImage(
             modifier = Modifier
                 .clip(RoundedCornerShape(10.dp))
-//                .background(Color.LightGray, RoundedCornerShape(10.dp))
+                .border(3.dp, Color(0xffE91E63), RoundedCornerShape(10.dp))
                 .fillMaxWidth()
                 .aspectRatio(4 / 3f),
-            beforeImage = imageBefore,
-            afterImage = imageAfter,
+            beforeImage = imageBefore2,
+            afterImage = imageAfter2,
+            progress = progress,
+            onProgressChange = {},
             contentScale = ContentScale.FillBounds
         ) {
-
-            val handlePosition = touchPosition.x
-            val posY: Int
-
-            val realPos = handlePosition - with(density) {
-                posY = (imageHeight / 3).roundToPx()
-                imageWidth.toPx() / 2
-            }
-
-
-            Canvas(modifier = Modifier.size(imageWidth, imageHeight)) {
-
-                val canvasWidth = size.width
-
-                val imagePosition = handlePosition.coerceIn(0f, canvasWidth)
-
-                drawLine(
-                    Color.White,
-                    strokeWidth = 1.5.dp.toPx(),
-                    start = Offset(imagePosition, 0f),
-                    end = Offset(imagePosition, size.height)
-                )
-
-            }
-
-            Image(
-                painter = painter,
-                contentDescription = null,
-                modifier = Modifier
-                    .offset {
-                        IntOffset(realPos.toInt(), posY)
-                    }
-                    .shadow(2.dp, CircleShape)
-                    .background(Color.White)
-                    .padding(2.dp)
+            Text(
+                "${(progress).roundToInt()}%",
+                fontSize = 50.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xff03A9F4)
             )
         }
-
-        Spacer(modifier = Modifier.height(20.dp))
-
-        BeforeAfterLayout(modifier = Modifier
-            .fillMaxWidth()
-            .aspectRatio(4 / 3f),
-            before = {
-                Image(
-                    painter = painterResource(id = R.drawable.landscape5),
-                    contentDescription = "",
-                    contentScale = ContentScale.FillBounds
-                )
-
-
-            },
-            after = {
-                Image(
-                    painter = painterResource(id = R.drawable.landscape5_after),
-                    contentDescription = "",
-                    contentScale = ContentScale.FillBounds
-                )
-            }
-        )
-
-
-        Spacer(modifier = Modifier.height(20.dp))
-
-        BeforeAfterLayout(modifier = Modifier,
-            before = {
-                Text(text = "Hello World", fontSize = 60.sp, color = Color.Red)
-
-            },
-            after = {
-                Text(text = "Hello World", fontSize = 60.sp, color = Color.Green)
-            }
-        )
     }
+}
+
+@Composable
+private fun Label(modifier: Modifier = Modifier, text: String) {
+    Text(
+        text = text,
+        color = Color.White,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Bold,
+        modifier = modifier
+            .background(Color.Black.copy(alpha = .4f), RoundedCornerShape(50))
+            .padding(8.dp)
+    )
 }

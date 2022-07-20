af9805a707780cacea9d1bac1aef06c5aeb113f9
Smart Tool Factory <smarttoolfactory@icloud.com>
2022-07-20 18:09:49 +0300

    add default before and after labels

--- image/src/main/java/com/smarttoolfactory/image/beforeafter/Label.kt
+++ image/src/main/java/com/smarttoolfactory/image/beforeafter/Label.kt
@@ -1,10 +1,12 @@
 package com.smarttoolfactory.image.beforeafter
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -24,5 +26,29 @@ fun Label(modifier: Modifier = Modifier, text: String) {
         modifier = modifier
             .background(Color.Black.copy(alpha = .5f), RoundedCornerShape(50))
             .padding(horizontal = 12.dp, vertical = 8.dp)
+    )
+}
+
+@Composable
+fun BoxScope.BeforeLabel(contentOrder: ContentOrder) {
+    Label(
+        text = "Before", modifier = Modifier
+            .padding(8.dp)
+            .align(
+                if (contentOrder == ContentOrder.BeforeAfter)
+                    Alignment.TopStart else Alignment.TopEnd
+            )
+    )
+}
+
+@Composable
+fun BoxScope.AfterLabel(contentOrder: ContentOrder) {
+    Label(
+        text = "After", modifier = Modifier
+            .padding(8.dp)
+            .align(
+                if (contentOrder == ContentOrder.BeforeAfter)
+                    Alignment.TopEnd else Alignment.TopStart
+            )
     )
 }

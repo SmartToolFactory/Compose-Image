3814471c041592525e561329e287aa4e907ffbb4
Smart Tool Factory <smarttoolfactory@icloud.com>
2022-07-21 15:02:48 +0300

    add OverLayStyle for setting default overlay properties

--- image/src/main/java/com/smarttoolfactory/image/beforeafter/DefaultOverlay.kt
+++ image/src/main/java/com/smarttoolfactory/image/beforeafter/DefaultOverlay.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -30,32 +31,26 @@ import com.smarttoolfactory.image.R
  * @param height of the [BeforeAfterImage] or [BeforeAfterLayout]. You should get height from
  * scope of these Composables and pass to calculate bounds correctly
  * @param position current position or progress of before/after
- * @param verticalThumbMove when true thumb can move vertically based on user touch
- * @param lineColor color if divider line
- * @param thumbBackgroundColor background color of thumb [Icon]
- * @param thumbTintColor tint color of thumb [Icon]
- * @param thumbShape shape of thumb [Icon]
- * @param thumbElevation elevation of thumb [Icon]
- * @param thumbResource drawable resource that should be used with thumb
- * @param thumbSize size of the thumb in dp
- * @param thumbPositionPercent vertical position of thumb if [verticalThumbMove] is false
- * It's between [0f-100f] to set thumb's vertical position in layout
  */
 @Composable
 internal fun DefaultOverlay(
     width: Dp,
     height: Dp,
     position: Offset,
-    verticalThumbMove: Boolean = false,
-    lineColor: Color = Color.White,
-    thumbBackgroundColor: Color = Color.White,
-    thumbTintColor: Color = Color.Gray,
-    thumbShape: Shape = CircleShape,
-    thumbElevation: Dp = 2.dp,
-    @DrawableRes thumbResource: Int = R.drawable.baseline_swap_horiz_24,
-    thumbSize: Dp = 36.dp,
-    @FloatRange(from = 0.0, to = 100.0) thumbPositionPercent: Float = 85f,
+    overlayStyle: OverlayStyle
 ) {
+
+    val verticalThumbMove = overlayStyle.verticalThumbMove
+    val dividerColor = overlayStyle.dividerColor
+    val dividerWidth = overlayStyle.dividerWidth
+    val thumbBackgroundColor = overlayStyle.thumbBackgroundColor
+    val thumbTintColor = overlayStyle.thumbTintColor
+    val thumbShape = overlayStyle.thumbShape
+    val thumbElevation = overlayStyle.thumbElevation
+    val thumbResource = overlayStyle.thumbResource
+    val thumbSize = overlayStyle.thumbSize
+    val thumbPositionPercent = overlayStyle.thumbPositionPercent
+
 
     var thumbPosX = position.x
     var thumbPosY = position.y
@@ -93,8 +88,8 @@ internal fun DefaultOverlay(
         Canvas(modifier = Modifier.fillMaxSize()) {
 
             drawLine(
-                lineColor,
-                strokeWidth = 1.5.dp.toPx(),
+                dividerColor,
+                strokeWidth = dividerWidth.toPx(),
                 start = Offset(linePosition, 0f),
                 end = Offset(linePosition, size.height)
             )
@@ -115,3 +110,31 @@ internal fun DefaultOverlay(
         )
     }
 }
+
+/**
+ * Values for styling [DefaultOverlay]
+ *  @param verticalThumbMove when true thumb can move vertically based on user touch
+ * @param dividerColor color if divider line
+ * @param dividerWidth width if divider line
+ * @param thumbBackgroundColor background color of thumb [Icon]
+ * @param thumbTintColor tint color of thumb [Icon]
+ * @param thumbShape shape of thumb [Icon]
+ * @param thumbElevation elevation of thumb [Icon]
+ * @param thumbResource drawable resource that should be used with thumb
+ * @param thumbSize size of the thumb in dp
+ * @param thumbPositionPercent vertical position of thumb if [verticalThumbMove] is false
+ * It's between [0f-100f] to set thumb's vertical position in layout
+ */
+@Immutable
+class OverlayStyle(
+    val dividerColor: Color = Color.White,
+    val dividerWidth: Dp = 1.5.dp,
+    val verticalThumbMove: Boolean = false,
+    val thumbBackgroundColor: Color = Color.White,
+    val thumbTintColor: Color = Color.Gray,
+    val thumbShape: Shape = CircleShape,
+    val thumbElevation: Dp = 2.dp,
+    @DrawableRes val thumbResource: Int = R.drawable.baseline_swap_horiz_24,
+    val thumbSize: Dp = 36.dp,
+    @FloatRange(from = 0.0, to = 100.0) val thumbPositionPercent: Float = 85f,
+)
